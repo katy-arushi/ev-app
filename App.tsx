71b5b52108307bec6f5d34b665bab670385eb2df
Arushi Katyal <katy.arushi@gmail.com>
2023-02-21 01:28:30 -0500

    add intial boilerplate from expo generate

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { styles } from './styles';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default function App(): JSX.Element {
+	const Stack = createNativeStackNavigator();
+
+	return (
+		<NavigationContainer>
+			<View style={styles.homeContainer}>
+				<WelcomeScreen />
+				<StatusBar style='auto' />
+			</View>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
