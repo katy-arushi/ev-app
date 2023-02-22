e198a334bf984618ee02cb42038ae2d475c80aef
Arushi Katyal <katy.arushi@gmail.com>
2023-02-21 21:19:34 -0500

    minor text adjustment, change View to SafeAreaView

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,21 +1,22 @@
-import { StyleSheet, View, Text } from 'react-native';
-import Button from '../components/Button';
-import { styles } from '../styles';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Button from '../components/Button';
+
+import { styles } from '../styles';
 
 const WelcomeScreen = (): JSX.Element => {
 	return (
-		<View>
+		<SafeAreaView style={styles.homeContainer}>
 			<Text style={styles.header}>
-				Welcome to <Text style={styles.span}>ev.energy</Text>
+				Welcome to <Text style={styles.logoName}>ev.energy</Text>
 			</Text>
 			<Text style={styles.text}>
-				Please press continue to find an electric vehicle charger near you
-      </Text>
-      <Button/>
-		</View>
+				Press continue to find an electric vehicle charger near you
+			</Text>
+			<Button />
+		</SafeAreaView>
 	);
 };
 
 export default WelcomeScreen;
-
